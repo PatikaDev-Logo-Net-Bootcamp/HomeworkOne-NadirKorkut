8e04049d935e31b0456c3ead63c8aa9006266b0d
nadirkorkut <korkutnadir@outlook.com>
2022-03-05 12:10:35 +0300

    first commit

---
+++ HomeworkOneNadirKorkut/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using HomeworkOneNadirKorkut.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeworkOneNadirKorkut.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var invalidUser = new ResponseViewModel
+                {
+                    Data = "null",
+                    Success = false,
+                    Error = "Hatalı Giriş"
+                };
+                TempData["response"] = "Data : " + invalidUser.Data + ", " + "Success : " + invalidUser.Success + ", " + "Error : " + invalidUser.Error;
+                return View();
+            }
+
+            var validUser = new ResponseViewModel
+            {
+                Data = "Giriş İşlemi Başarılı",
+                Success = true,
+                Error = "null"
+            };
+
+            TempData["response"] = "Data : " + validUser.Data + ", " + "Success : " + validUser.Success + ", " + "Error : " + validUser.Error;
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HomeworkOneNadirKorkut/Models/LoginViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeworkOneNadirKorkut.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Ad Alanı Boş Geçilemez!")]
+        [Display(Name = "Ad")]
+        [RegularExpression("^([a-zA-Z])+$", ErrorMessage = "Adınız sadece harflerden oluşmalıdır")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Soyad Alanı Boş Geçilemez!")]
+        [Display(Name = "Soyad")]
+        [RegularExpression("^([a-zA-Z])+$", ErrorMessage = "Soyadınız sadece harflerden oluşmalıdır")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email Alanı Boş Geçilemez!")]
+        [Display(Name = "E-Posta")]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]*@[A-Za-z0-9.-]*\.[A-Za-z0-9-]{2,}$", ErrorMessage = "Lütfen geçerli formatta bir email adresi giriniz.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre Alanı Boş Geçilemez!")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.{0,8}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$", ErrorMessage = "Şifreniz en fazla 8 karakterden oluşmalıdır ve En az 1 Büyük Harf, 1 özel karakter,1 rakamdan oluşmalıdır.")]
+        public string Password { get; set; }
+    }
+}
